a5a0e5b713c141c3ce8430e50e8daf67cdf0495d
ayobami_ogundiran <ayobami_ogundiran@yahoo.com>
2024-10-15 14:15:15 +0100

    Re-wrote metadata texts

--- assets/js/components/ArticleMetaData.js
+++ assets/js/components/ArticleMetaData.js
@@ -1,7 +1,7 @@
 export function ArticleMetaData({ metadata, date }){
   return `
     <small class="text-gray-300">
-    Word Count: ${ metadata.wordCount }, Read Time: ${ metadata.readTime } min, Published on: ${new Date(date).toLocaleDateString()}
+      ${ metadata.wordCount } words, ${ metadata.readTime } min read, Published on: ${new Date(date).toLocaleDateString()}
     </small>
   `;
 }
